d9dfc4cab5ee5927ecd1cf1821b9b7bd0da6b6e4
levtus <i@alexander.systems>
2023-10-05 20:30:40 -0500

    Rebalance POIs

--- src/main/java/com/sudolev/villagelife/ModItems/ModItems.java
+++ src/main/java/com/sudolev/villagelife/ModItems/ModItems.java
@@ -1,19 +1,26 @@
 package com.sudolev.villagelife.ModItems;
 
 import com.sudolev.villagelife.VillageLife;
+import com.sudolev.villagelife.villager.ModVillagers;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SpawnEggItem;
+import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, VillageLife.MODID);
 
     public static final RegistryObject<Item> MECHANICAL_ENGINEER_SPAWN_EGG = ITEMS.register("mechanical_engineer_spawn_egg",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTabs.VILLAGE_LIFE_TAB)));
+            () -> new ForgeSpawnEggItem(([WHAT GOES HERE], 111111111, 111111111, new Item.Properties().tab(ModCreativeModeTabs.VILLAGE_LIFE_TAB)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
